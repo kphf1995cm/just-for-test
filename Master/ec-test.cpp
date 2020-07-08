6aa3934687f1cb347ce89041255695f2f8abe7a0
kuangpeng <kp18@mails.tsinghua.edu.cn>
2020-07-08 14:21:38 +0800

    add encode decode func but still have problem in decode

--- Master/ec-test.cpp
+++ Master/ec-test.cpp
@@ -1,7 +1,37 @@
 # include "ec.h"
 # include<iostream>
+# include<string>
+# include<vector>
+using namespace std;
+
+// g++ ec-test.cpp -o ect -lerasurecode -ldl
+
+void test_encode_decode(int ds){
+    EC ec;
+    std::string value(ds,'a');
+    std::vector<std::string> encodedData;
+    std::vector<std::string> encodedParity;
+    uint64_t fragmentLen;
+    ec.encode(value,encodedData,encodedParity,fragmentLen);
+    std::string restoreValue;
+    std::vector<std::string> fragments;
+    fragments.push_back(encodedData[0]);
+    fragments.push_back(encodedData[1]);
+    fragments.push_back(encodedParity[0]);
+    fragments.push_back(encodedParity[2]);
+    ec.decode(fragments,fragmentLen,restoreValue);
+    std::string idxFragment;
+    ec.reconstructFragment(fragments,fragmentLen,2,idxFragment);
+    cout<<value.compare(restoreValue)<<" ";
+    cout<<idxFragment.compare(encodedData[2])<<endl;
+}
 
 int main(){
-    EC ec;
-    ec.test_encode();
+    //EC ec;
+    for(int i=200;i<=250;i++){
+        //ec.test_encode(i);
+        //std::cout<<std::endl;
+        test_encode_decode(i);
+    }
 }
+
--- Master/ec.h
+++ Master/ec.h
@@ -3,6 +3,9 @@
 # include <erasurecode.h>
 # include <assert.h>
 # include <stdio.h>
+# include <iostream>
+# include <string>
+# include <vector>
 // https://github.com/intel/isa-l
 // https://opendev.org/openstack/liberasurecode/src/branch/master/doc/api_definition.md
 // https://github.com/openstack/liberasurecode/blob/master/include/erasurecode/erasurecode.h
@@ -33,6 +36,9 @@ struct ec_args {
 */
 
 // g++ ec-test.cpp -o ect -lerasurecode -ldl
+// https://blog.csdn.net/sean4m/article/details/55211492
+
+// requirement: std=c++11 or std=gnu++11
 
 char *create_buffer(int size, int fill){
     char *buf = (char *)malloc(size);
@@ -42,31 +48,108 @@ char *create_buffer(int size, int fill){
 
 class EC{
     private:
-        int dataNodeNum;
-        int codeNodeNum;
-        int ecDescriptor;
+
+        ec_backend_id_t ecbi;
+        struct ec_args args;
+        int ecDesc;
+
     public:
-        EC(){
+
+        EC(int _k,int _m,int _w,int _hd,ec_backend_id_t _ecbi){
+            args = {
+                .k = _k,
+                .m = _m,
+                .w = _w,
+                .hd = _hd,
+            };
+            ecbi=_ecbi;
+            init();
         }
-        void init(){
-            struct ec_args isa_l_44_args = {
+
+        EC(){
+            args = {
                 .k = 4,
                 .m = 4,
                 .w = 8,
                 .hd = 5,
             };
-            ecDescriptor=liberasurecode_instance_create(EC_BACKEND_ISA_L_RS_VAND,&isa_l_44_args);
-            liberasurecode_instance_destroy(ecDescriptor);
+            ecbi=EC_BACKEND_ISA_L_RS_VAND;
+            init();
         }
 
-        void encode(){
-
+        ~EC(){
+            liberasurecode_instance_destroy(ecDesc);
         }
-        void decode(){
 
+        void init(){
+            ecDesc=liberasurecode_instance_create(ecbi,&args);
         }
-        void test_encode(){
 
+        void encode(std::string &value,std::vector<std::string> &encodedData,std::vector<std::string> &encodedParity,uint64_t &fragmentLen){
+            int orig_data_size=value.size();
+            char *orig_data=new char[value.size()+1];
+            strcpy(orig_data,value.c_str());
+            char **encoded_data = NULL;
+            char **encoded_parity = NULL;
+
+            assert(orig_data != NULL);
+            int rc=liberasurecode_encode(ecDesc, orig_data, orig_data_size,&encoded_data, &encoded_parity, &fragmentLen);
+            delete []orig_data;
+            assert(rc==0);
+            int ed_size=args.k;
+            int ep_size=args.m;
+            for(int i=0;i<ed_size;i++){
+                std::string eds(encoded_data[i]);
+                encodedData.push_back(eds);
+            }
+            for(int i=0;i<ep_size;i++){
+                std::string eps(encoded_parity[i]);
+                encodedParity.push_back(eps);
+            }
+            liberasurecode_encode_cleanup(ecDesc,encoded_data,encoded_parity);
+        }
+
+        void decode(std::vector<std::string> fragments,uint64_t fragmentLen, std::string &oriValue){
+            assert(fragments.size()>=args.k);
+            char** c_fragments=new char*[fragments.size()];
+            for(int i=0;i<fragments.size();i++){
+                c_fragments[i]=new char[fragments[i].size()+1];
+                strcpy(c_fragments[i],fragments[i].c_str());
+            }
+            char *out_data=NULL;
+            uint64_t out_data_len=0;
+            std::cout<<"fragments size: "<<fragments.size()<<" fragmentLen: "<<fragmentLen<<std::endl;
+            int rc=liberasurecode_decode(ecDesc,c_fragments,fragments.size(),fragmentLen,0,&out_data,&out_data_len);
+            std::cout<<"rc: "<<rc<<std::endl;
+            for(int i=0;i<fragments.size();i++){
+                delete []c_fragments[i];
+            }
+            delete []c_fragments;
+            //assert(rc==0);
+            oriValue.assign(out_data);
+            liberasurecode_decode_cleanup(ecDesc,out_data);
+        }
+
+        void reconstructFragment(std::vector<std::string> fragments,uint64_t fragmentLen,int idx,std::string &idxFragment){
+            assert(fragments.size()>=args.k);
+            char** c_fragments=new char*[fragments.size()];
+            for(int i=0;i<fragments.size();i++){
+                c_fragments[i]=new char[fragments[i].size()+1];
+                strcpy(c_fragments[i],fragments[i].c_str());
+            }
+            char *out_fragment=new char[fragmentLen];
+            int rc=liberasurecode_reconstruct_fragment(ecDesc,c_fragments,fragments.size(),fragmentLen,idx,out_fragment);
+            assert(rc!=0);
+            for(int i=0;i<fragments.size();i++){
+                delete []c_fragments[i];
+            }
+            delete []c_fragments;
+            idxFragment.assign(out_fragment);
+            delete []out_fragment;
+        }
+
+        void test_encode(int orig_data_size){
+            //printf("orig_data_size:%d\t",orig_data_size);
             struct ec_args isa_l_44_args = {
                 .k = 4,
                 .m = 4,
@@ -76,17 +159,16 @@ class EC{
             int desc=liberasurecode_instance_create(EC_BACKEND_ISA_L_RS_VAND,&isa_l_44_args);
 
             int rc=0;
-            int orig_data_size = 100;
             char *orig_data = create_buffer(orig_data_size, 'x');
             char **encoded_data = NULL, **encoded_parity = NULL;
             uint64_t fragment_len = 0;
-            printf("ori_data:%s\n",orig_data);
+            //printf("ori_data:%s\n",orig_data);
             
             assert(orig_data != NULL);
             rc=liberasurecode_encode(desc, orig_data, orig_data_size,&encoded_data, &encoded_parity, &fragment_len);
             assert(rc ==0);
             //liberasurecode_encode_cleanup(desc,encoded_data,encoded_parity);
-            printf("encoded_data[2]:%s\n",encoded_data[2]);
+            //printf("encoded_data[2]:%s\n",encoded_data[2]);
             int num_fragments=4;
             char **fragments = (char **)malloc(num_fragments*sizeof(char*));
             fragments[0]=encoded_data[0];
@@ -97,9 +179,10 @@ class EC{
             uint64_t out_data_len=0;
             rc=liberasurecode_decode(desc,fragments,num_fragments,fragment_len,0,&out_data,&out_data_len);
             assert(rc==0);
-            printf("out_data:%s\n",out_data);
+            //printf("out_data:%s\n",out_data);
             if(strcmp(orig_data,out_data)==0){
-                printf("encode/decode success\n");
+                //printf("encode/decode success\t");
+                printf("%d ",orig_data_size);
             }
             //liberasurecode_decode_cleanup(desc,out_data);
 
@@ -107,10 +190,10 @@ class EC{
             int destination_idx=2;
             rc=liberasurecode_reconstruct_fragment(desc,fragments,num_fragments,fragment_len,destination_idx,out_fragment);
             assert(rc==0);
-            printf("out_fragment:%s\n",out_fragment);
-            if(strcmp(encoded_data[2],out_fragment)==0){
-                printf("reconstruct fragment success\n");
-            }
+            //printf("out_fragment:%s\n",out_fragment);
+            //if(strcmp(encoded_data[2],out_fragment)==0){
+            //    printf("reconstruct fragment success\t");
+            //}
 
             free(fragments);
             free(out_fragment);
